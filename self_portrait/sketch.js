a7f30485b85d64839e4cac275ec6d1e987d1f57d
moequinn <moequinn50@gmail.com>
2018-09-06 00:04:21 -0400

    changes to sketch and colors

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,9 +1,27 @@
+// self portrait
 function setup() {
-	createCanvas(640, 400);
+	createCanvas(640, 360);
 }
 
 function draw() {
-	background(220);
-	ellipse(120, 80, 400);
-    
+	background("white");
+	noStroke();
+	
+	// face
+	fill("lightgreen");
+	rect(100, 50, 200, 250);
+	
+	// eyes
+	fill("blue");
+	ellipse(150, 150, 50, 100);
+	ellipse(250, 150, 40, 80);
+	
+	// ears
+	fill("yellow");
+	ellipse(150, 150, 40, 40);
+	ellipse(250, 150, 30, 30);
+
+	// mouth
+	stroke("blue");
+	line(150, 250, 250, 280);
 }
