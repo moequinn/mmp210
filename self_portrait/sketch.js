c3239fbcea64fd9258a8a9c54604ffd02ad27af3
moequinn <moequinn50@gmail.com>
2018-09-09 15:14:57 -0400

    more changes to the shapes and positions

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -11,27 +11,32 @@ function draw() {
 	fill("blue");
 	ellipse(320, 340, 250, 250);
     
+    // front_layer
+    fill("White")
+    triangle(260, 260, 320, 360, 380, 260);
+    
 	// face
 	fill("tan");
 	ellipse(320, 180, 200, 250);
 	
 	// outter_eyes 
 	fill("white");
-	ellipse(280, 150, 50, 100); //left
-	ellipse(350, 150, 40, 80); //right
+	ellipse(280, 150, 40, 70); //my_left
+	ellipse(350, 150, 40, 70); //my_right
 	
 	// mid_eyes
 	fill("brown");
-	ellipse(280, 150, 40, 40); //left
-	ellipse(350, 150, 30, 30); //right
+	ellipse(280, 150, 30, 40); //my_left
+	ellipse(350, 150, 30, 40); //my_right
     
     // pupil
 	fill("black");
-	ellipse(280, 150, 30, 30); //left
-	ellipse(350, 150, 20, 20); //right
+	ellipse(280, 150, 20, 20); //my_left
+	ellipse(350, 150, 20, 20); //my_right
 
 	// mouth
 	stroke("brown");
-	line(270, 270, 350, 290);
+	line(270, 250, 350, 270);
+    
     
 }
