465a5225a22d202f58492278f5d60245c1b1fbbe
moequinn <moequinn50@gmail.com>
2018-09-22 14:58:10 -0400

    rearrange cooments

--- self_portrait_1/sketch.js
+++ self_portrait_1/sketch.js
@@ -4,8 +4,8 @@ function setup() {
 }
 
 function draw() {
-       background(204, 248, 212, 0.6);
-       noStroke();
+    background(204, 248, 212, 0.6);
+    noStroke();
 	
     var bodyColor = "blue"
     var bodyx = 320
@@ -13,9 +13,9 @@ function draw() {
     var bodywidth = 250
     var bodyheight = 250
     
-    // body
-       fill(bodyColor);
-       ellipse(bodyx, bodyy, bodywidth, bodyheight);
+// body
+    fill(bodyColor);
+    ellipse(bodyx, bodyy, bodywidth, bodyheight);
     
     var frontlayercolor = "white"
     var frontlayerx1 = 260
@@ -26,9 +26,9 @@ function draw() {
     var frontlayery3 = 260
     
     
-    // front_layer
-       fill(frontlayercolor)
-       triangle(frontlayerx1, frontlayery1, frontlayerx2, frontlayery2, frontlayerx3, frontlayery3);
+// front layer
+    fill(frontlayercolor)
+    triangle(frontlayerx1, frontlayery1, frontlayerx2, frontlayery2, frontlayerx3, frontlayery3);
     
     var haircolor ="black"
     var hairx = 320
@@ -36,8 +36,8 @@ function draw() {
     var hairwidth = 185
     var hairheight = 210
     
-    // hair
-	fill(haircolor)
+// hair
+    fill(haircolor)
 	ellipse(hairx , hairy, hairwidth, hairheight);
     
     var facecolor = "tan"
@@ -46,7 +46,7 @@ function draw() {
     var facewidth = 180
     var faceheight = 210
     
-    // face
+// face
 	fill(facecolor);
 	ellipse(facex, facey, facewidth, faceheight);
     
@@ -60,7 +60,7 @@ function draw() {
     var oeyesrw = 40
     var oeyesrh = 70
 	
-	// outter_eyes 
+// outter eyes 
 	fill(oeyescolor);
 	ellipse(oeyeslx, oeyesly, oeyeslw, oeyeslh); //my_left
 	ellipse(oeyesrx, oeyesry, oeyesrw, oeyesrh); //my_right
@@ -75,7 +75,7 @@ function draw() {
     var mideyesrw = 30
     var mideyesrh = 40
 	
-	// mid_eyes
+// mid eyes
 	fill(mideyescolor);
 	ellipse(mideyeslx, mideyesly, mideyeslw, mideyeslh); //my_left
 	ellipse(mideyesrx, mideyesry, mideyesrw, mideyesrh); //my_right
@@ -90,7 +90,7 @@ function draw() {
     var pupilrw = 20
     var pupilrh = 20
     
-    // pupil
+// pupil
 	fill(pupilcolor);
 	ellipse(pupillx, pupilly, pupillw, pupillh); //my_left
 	ellipse(pupilrx, pupilry, pupilrw, pupilrh); //my_right
@@ -101,7 +101,7 @@ function draw() {
     var mouthw = 350
     var mouthh = 270
 
-    // mouth
+// mouth
        stroke(mouthcolor);
        line(mouthx, mouthy, mouthw, mouthh);   
 }
