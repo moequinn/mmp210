4ceaaffbcc2c2898732b081d754652277a3f29be
Mohammad103095 <42847228+Mohammad103095@users.noreply.github.com>
2018-09-25 23:26:19 -0400

    interaction and position

--- self_portrait_2/sketch.js
+++ self_portrait_2/sketch.js
@@ -22,61 +22,61 @@ function draw() {
 // end of background code  
 	
 // body
-    var bodyColor = "blue"
-    var bodyx = 320
-    var bodyy = 340
-    var bodywidth = 250
-    var bodyheight = 250
+    var bodyColor = "blue";
+    var bodyx = 320;
+    var bodyy = 340;
+    var bodywidth = 250;
+    var bodyheight = 250;
     
     fill(bodyColor);
     ellipse(bodyx, bodyy, bodywidth, bodyheight); 
 // end of body code
     
 // front layer
-    var frontlayercolor = "white"
-    var frontlayerx1 = 260
-    var frontlayery1 = 260
-    var frontlayerx2 = 320
-    var frontlayery2 = 360
-    var frontlayerx3 = 380
-    var frontlayery3 = 260
+    var frontlayercolor = "white";
+    var frontlayerx1 = 260;
+    var frontlayery1 = 260;
+    var frontlayerx2 = 320;
+    var frontlayery2 = 360;
+    var frontlayerx3 = 380;
+    var frontlayery3 = 260;
     
-    fill(frontlayercolor)
+    fill(frontlayercolor);
     triangle(frontlayerx1, frontlayery1, frontlayerx2, frontlayery2, frontlayerx3, frontlayery3); 
 // end of front layer code
 
 // hair
-    var haircolor ="black"
-    var hairx = 320
-    var hairy = 180
-    var hairwidth = 185
-    var hairheight = 210
+    var haircolor ="black";
+    var hairx = 320;
+    var hairy = 180;
+    var hairwidth = 185;
+    var hairheight = 210;
 
-    fill(haircolor)
+    fill(haircolor);
 	ellipse(hairx , hairy, hairwidth, hairheight); 
 // end of hair code
     
 // face
-    var facecolor = "tan"
-    var facex = 320
-    var facey = 190
-    var facewidth = 180
-    var faceheight = 210
+    var facecolor = "tan";
+    var facex = 320;
+    var facey = 190;
+    var facewidth = 180;
+    var faceheight = 210;
     
 	fill(facecolor);
 	ellipse(facex, facey, facewidth, faceheight); 
 // end of face code
     
 // outter eyes
-    var oeyescolor = "white"
-    var oeyeslx = 280
-    var oeyesly = 170
-    var oeyeslw = 40
-    var oeyeslh = 70
-    var oeyesrx = 350
-    var oeyesry = 170
-    var oeyesrw = 40
-    var oeyesrh = 70
+    var oeyescolor = "white";
+    var oeyeslx = 280;
+    var oeyesly = 170;
+    var oeyeslw = 40;
+    var oeyeslh = 70;
+    var oeyesrx = 350;
+    var oeyesry = 170;
+    var oeyesrw = 40;
+    var oeyesrh = 70;
 	 
 	fill(oeyescolor);
 	ellipse(oeyeslx, oeyesly, oeyeslw, oeyeslh); //my_left
@@ -84,15 +84,15 @@ function draw() {
 // end of outter eyes code
     
 // mid eyes
-    var mideyescolor = "brown"
-    var mideyeslx = 280
-    var mideyesly = 170
-    var mideyeslw = 30
-    var mideyeslh = 40
-    var mideyesrx = 350
-    var mideyesry = 170
-    var mideyesrw = 30
-    var mideyesrh = 40
+    var mideyescolor = "brown";
+    var mideyeslx = 280;
+    var mideyesly = 170;
+    var mideyeslw = 30;
+    var mideyeslh = 40;
+    var mideyesrx = 350;
+    var mideyesry = 170;
+    var mideyesrw = 30;
+    var mideyesrh = 40;
 	
 	fill(mideyescolor);
 	ellipse(mideyeslx, mideyesly, mideyeslw, mideyeslh); //my_left
@@ -100,27 +100,27 @@ function draw() {
 // end of mid eyes code
     
 // pupil
-    var pupilcolor = "black"
-    var pupillx = 280
-    var pupilly = 170
-    var pupillw = 20
-    var pupillh = 20
-    var pupilrx = 350
-    var pupilry = 170
-    var pupilrw = 20
-    var pupilrh = 20
+    var pupilcolor = "black";
+    var pupillx = mouseX;
+    var pupilly = mouseY;
+    var pupillw = 20;
+    var pupillh = 20;
+    var pupilrx = mouseX;
+    var pupilry = mouseY;
+    var pupilrw = 20;
+    var pupilrh = 20;
     
 	fill(pupilcolor);
-	ellipse(pupillx, pupilly, pupillw, pupillh); //my_left
-	ellipse(pupilrx, pupilry, pupilrw, pupilrh); //my_right
+	ellipse(mouseX, mouseY, pupillw, pupillh); //my_left
+	ellipse(mouseX + 50, mouseY, pupilrw, pupilrh); //my_right
 // end of pupil code
     
 // mouth
-    var mouthcolor = "brown"
-    var mouthx = 270
-    var mouthy = 250
-    var mouthw = 350
-    var mouthh = 270
+    var mouthcolor = "brown";
+    var mouthx = 270;
+    var mouthy = 250;
+    var mouthw = 350;
+    var mouthh = 270;
 
     stroke(mouthcolor);
     line(mouthx, mouthy, mouthw, mouthh);
