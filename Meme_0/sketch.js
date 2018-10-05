611be84f4026def0ac6aac97837ad16b3041d4dc
Moe Quinn <moequinn50@gmail.com>
2018-10-05 00:08:51 -0400

    add

--- Meme_0/sketch.js
+++ Meme_0/sketch.js
@@ -5,16 +5,28 @@
     mmp 210 week 5
 */
 
+//("squirrel-2936889_1280.jpg")
+
+var squirrel; // global
+
+function preload() {
+    squirrel = loadImage("squirrel-2936889_1280.jpg");
+}
+
 function setup() {
     createCanvas(500, 500);
 }
 
 function draw() {
     background(0);
+    
+    image(squirrel, 0, 0, width, height);
+    //image(squirrel, 0, 0, width/2, height);
+    //image(squirrel, width/2, 0, width/2, height);
+    
     fill(255);
     textAlign(CENTER, CENTER);
-    textSize(100);
+    textSize(50);
     textFont("Comic Sans MS");
-    text("Rabbit", width/2, height/2);
+    text("Did you said Nuts", width/2, height/2);
 }
-
